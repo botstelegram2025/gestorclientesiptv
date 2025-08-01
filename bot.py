54d0dfa7698c5c6cc8ab6e85d007cc587306a485
botstelegram2025 <marquescreperia@gmail.com>
2025-08-01 11:42:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,6 +60,10 @@ async def main():
 
     logger.info("Bot iniciado com sucesso!")
     await app.run_polling()
-
+  
 if __name__ == '__main__':
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    asyncio.get_event_loop().run_until_complete(main())
+
